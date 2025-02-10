38e336c3da7d531eb4c5fc1d1b4eddd68b8c3317
k-r-abhishek <abhishekkrcd@gmail.com>
2025-02-10 15:55:08 +0530

    learning basics

---
+++ 01-first-class/Basics.java
@@ -0,0 +1,19 @@
+/*
+ * 1. Basics: Hello World and Variables
+ */
+public class Basics {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // Printing Output
+        
+        // Variable declaration
+        int age = 20;
+        double pi = 3.1416;
+        char grade = 'A';
+        boolean isJavaFun = true;
+        
+        System.out.println("Age: " + age);
+        System.out.println("Pi Value: " + pi);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Java Fun? " + isJavaFun);
+    }
+}
---
+++ 01-first-class/PrimitiveTypes.java
@@ -0,0 +1,24 @@
+/*
+ * 3. Primitive Data Types
+ */
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        byte smallNumber = 100;
+        short shortNumber = 32000;
+        int integerNumber = 50000;
+        long largeNumber = 9876543210L;
+        float decimalNumber = 5.75f;
+        double largeDecimalNumber = 19.99;
+        char letter = 'J';
+        boolean isCodingFun = true;
+        
+        System.out.println("Byte: " + smallNumber);
+        System.out.println("Short: " + shortNumber);
+        System.out.println("Integer: " + integerNumber);
+        System.out.println("Long: " + largeNumber);
+        System.out.println("Float: " + decimalNumber);
+        System.out.println("Double: " + largeDecimalNumber);
+        System.out.println("Char: " + letter);
+        System.out.println("Boolean: " + isCodingFun);
+    }
+}
