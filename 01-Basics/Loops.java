59b00adcae015963968e5d1c8e72b332a535dd93
k-r-abhishek <abhishekkrcd@gmail.com>
2025-02-18 22:32:56 +0530

    Go through them.

---
+++ 01-Basics/Loops.java
@@ -0,0 +1,18 @@
+/*
+ * 7. Loops: while and for
+ */
+public class Loops {
+    public static void main(String[] args) {
+        System.out.println("While Loop Example:");
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("Count: " + i);
+            i++;
+        }
+        
+        System.out.println("For Loop Example:");
+        for (int j = 1; j <= 5; j++) {
+            System.out.println("Iteration: " + j);
+        }
+    }
+}
---
+++ 01-Basics/TypeCasting.java
@@ -0,0 +1,12 @@
+/*
+ * 4. Type Casting
+ */
+public class TypeCasting {
+    public static void main(String[] args) {
+        int num = (int) 67.854f; // Explicit Casting
+        double value = 100; // Implicit Casting
+        
+        System.out.println("Explicit Casting: " + num);
+        System.out.println("Implicit Casting: " + value);
+    }
+}
